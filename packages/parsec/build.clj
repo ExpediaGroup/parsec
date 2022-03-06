b216c08d4cc42cd2ca2264671dace224e576618e
Dave Bauman <dave@davebauman.io>
2022-03-06 11:10:24 -0500

    chore(workflow): Try release please

--- packages/parsec/build.clj
+++ packages/parsec/build.clj
@@ -2,7 +2,7 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'ExpediaGroup/parsec)
-(def version (slurp "../../VERSION"))
+(def version (slurp "../../version.txt"))
 (def class-dir "target/classes")
 (def basis (b/create-basis {:project "deps.edn"}))
 (def jar-file (format "target/%s-%s.jar" (name lib) version))
