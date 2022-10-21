2f7332ab8f16552d82f73b95c38c8a873089e185
Dave Bauman <dave@davebauman.io>
2022-10-20 22:26:06 -0400

    fix: Fix mock input tests

--- packages/parsec/test/parsec/statements/input_statement_test.clj
+++ packages/parsec/test/parsec/statements/input_statement_test.clj
@@ -2,67 +2,40 @@
   (:require [clojure.test :refer :all]
             [parsec.test-helpers :refer :all]
             [parsec.test-datasets :refer :all]
-            [parsec.parser :refer :all]
-            [parsec.statements :refer [input-statement mock-dataset]]))
+            [parsec.parser :refer :all]))
 
 (deftest input-statement-parser-test
   (testing-parser!
-    "with no type" :input-statement "input")
+   "with no type" :input-statement "input")
 
   (testing-parser!
-    "with assignment" :input-statement "input=mock")
+   "with assignment" :input-statement "input=mock")
 
   (testing-parser!
-    "with quoted source type" :input-statement "input 'mock'")
+   "with quoted source type" :input-statement "input 'mock'")
 
   (testing-parser!
-    "with quoted source type" :input-statement "input [mock]")
+   "with quoted source type" :input-statement "input [mock]")
 
   (testing-parser!
-    "with quoted source type" :input-statement "input \"mock\"")
+   "with quoted source type" :input-statement "input \"mock\"")
 
   (testing-parser
-    "with no options" :input-statement
-    "input mock" [:input-statement :mock [:function :tolowercasemap]])
+   "with no options" :input-statement
+   "input mock" [:input-statement :mock [:function :tolowercasemap]])
 
   (testing-parser
-    "with one option" :input-statement
-    "input mocklarge n='20'" [:input-statement :mocklarge [:function :tolowercasemap "n" [:expression "20"]]])
+   "with one option" :input-statement
+   "input mocklarge n='20'" [:input-statement :mocklarge [:function :tolowercasemap "n" [:expression "20"]]])
 
   (testing-parser
-    "with two options" :input-statement
-    "input mock n='20' m=40" [:input-statement :mock [:function :tolowercasemap "n" [:expression "20"] "m" [:expression 40]]])
+   "with two options" :input-statement
+   "input mock n='20' m=40" [:input-statement :mock [:function :tolowercasemap "n" [:expression "20"] "m" [:expression 40]]])
 
   (testing-parser
-    "with three options" :input-statement
-    "input test x=\"1\" y=2 z=true" [:input-statement :test [:function :tolowercasemap "x" [:expression "1"] "y" [:expression 2] "z" [:expression true]]])
+   "with three options" :input-statement
+   "input test x=\"1\" y=2 z=true" [:input-statement :test [:function :tolowercasemap "x" [:expression "1"] "y" [:expression 2] "z" [:expression true]]])
 
   (testing-parser
-    "with three options and commas" :input-statement
-    "input test x=\"1\",y=2,z=true" [:input-statement :test [:function :tolowercasemap "x" [:expression "1"] "y" [:expression 2] "z" [:expression true]]]))
-
-(deftest input-statement-test
-  (testing-statement
-    "mock input"
-    (input-statement :mock {})
-    nil
-    mock-dataset)
-
-  (testing "mock input with no option"
-    (let [result (:current-dataset ((input-statement :mock {}) nil))]
-      (is (= 5 (count result)))))
-
-  (testing "mock N input with 1"
-    (let [result (:current-dataset ((input-statement :mock { :n 1 }) nil))]
-      (is (= 1 (count result)))))
-
-  (testing "mock N input with 20"
-    (let [result (:current-dataset ((input-statement :mock { :n 20 }) nil))]
-      (is (= 20 (count result)))))
-
-  (testing "mock N input with 20"
-    (let [result (:current-dataset ((input-statement :mock { :n "20"}) nil))]
-      (is (= 20 (count result)))))
-
-  (testing "invalid input"
-    (is (thrown? Exception (input-statement "bogus-input" {})))))
+   "with three options and commas" :input-statement
+   "input test x=\"1\",y=2,z=true" [:input-statement :test [:function :tolowercasemap "x" [:expression "1"] "y" [:expression 2] "z" [:expression true]]]))
